219478eea7a12639fcf357a8fc871f5460fe9e7e
hedenicapedrosa <hedenica@hotmail.com>
2021-04-15 09:49:46 -0300

    ♻️  refactor form functions | add cashback

--- pages/form/form.js
+++ pages/form/form.js
@@ -1,130 +1,167 @@
-const template = document.querySelector('[data-js="template"]')
-const form = document.querySelector('[data-js="form"]')
-const plusBtn = document.querySelector('[data-js="plus-btn"]')
-const submitBtn = document.querySelector('[data-js="submit-btn"]')
-const total = document.querySelector('[data-js="total"]')
+const template = document.querySelector('[data-js="template"]');
+const form = document.querySelector('[data-js="form"]');
+const plusBtn = document.querySelector('[data-js="plus-btn"]');
+const submitBtn = document.querySelector('[data-js="submit-btn"]');
+const total = document.querySelector('[data-js="total"]');
 
-const url_to_fetch = 'https://606467ecf0919700177859ac.mockapi.io/api/orders'
+const url_to_fetch = "https://606467ecf0919700177859ac.mockapi.io/api/orders";
 
 function cloneElement(options) {
-  const templateClone = template.content.firstElementChild.cloneNode(true)
+  const templateClone = template.content.firstElementChild.cloneNode(true);
 
-  const minusBtn = templateClone.querySelector('[data-js="minus-btn"]')
-  const priceInput = templateClone.querySelector('[data-js="input-price"]')
-  
-  templateClone.setAttribute('data-id', options.id)
-  minusBtn.setAttribute('data-id', options.id)
-  plusBtn.setAttribute('data-id', options.id)
-  templateClone.setAttribute('data-id', options.id)
-  priceInput.setAttribute('data-id', options.id)
+  const minusBtn = templateClone.querySelector('[data-js="minus-btn"]');
+  const priceInput = templateClone.querySelector('[data-js="input-price"]');
 
-  minusBtn.addEventListener('click', handleRemoveProduct, false)
-  priceInput.addEventListener('input', handleInputPrice, false)
+  templateClone.setAttribute("data-id", options.id);
+  minusBtn.setAttribute("data-id", options.id);
+  plusBtn.setAttribute("data-id", options.id);
+  templateClone.setAttribute("data-id", options.id);
+  priceInput.setAttribute("data-id", options.id);
 
-  form.appendChild(templateClone)
+  minusBtn.addEventListener("click", handleRemoveProduct, false);
+  priceInput.addEventListener("input", handleInputPrice, false);
+
+  form.appendChild(templateClone);
 }
 
-plusBtn.addEventListener('click', handleAddProduct, false)
+plusBtn.addEventListener("click", handleAddProduct, false);
 
-form.addEventListener('submit', submit, false)
+form.addEventListener("submit", submit, false);
 
 function handleInputPrice(e) {
-  e.target.value = maskPrice(e.target.value)
-  updateTotalPrice()
+  e.target.value = maskPrice(e.target.value);
+  updateTotalPrice();
 }
 
 function handleAddProduct(e) {
-  const elementId = Number(e.currentTarget.getAttribute('data-id'))
+  const elementId = Number(e.currentTarget.getAttribute("data-id"));
 
-  cloneElement({ id: elementId + 1 })
+  cloneElement({ id: elementId + 1 });
 }
 
 function updateTotalPrice() {
-  const inputElements = document.querySelectorAll('[data-js="input-price"]')
+  const inputElements = document.querySelectorAll('[data-js="input-price"]');
 
-  const arrayElements = [...inputElements]
+  const arrayElements = [...inputElements];
 
   if (inputElements.length === 0) {
-    total.innerText = formatCurrency(0)
+    total.innerText = formatCurrency(0);
   }
-  
+
   const totalValue = arrayElements.reduce((acc, input) => {
-    const clearInputValue = cleanValue(input.value)
+    const clearInputValue = cleanValue(input.value);
 
-    return acc + clearInputValue
-  }, 0)
+    return acc + clearInputValue;
+  }, 0);
 
-  total.innerText = formatCurrency(totalValue)
+  total.innerText = formatCurrency(totalValue);
 }
 
 function maskPrice(value) {
-  return formatCurrency(cleanValue(value))
+  return formatCurrency(cleanValue(value));
 }
 
-const cleanValue = (value) => Number(value.replace(/\D+/g, '') / 100)
+const cleanValue = (value) => Number(value.replace(/\D+/g, "") / 100);
 
 function formatCurrency(value) {
-  return new Intl.NumberFormat('pt-BR', {
-    style: 'currency',
-    currency: 'BRL',
-  }).format(value)
+  return new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(value);
 }
 
 function handleRemoveProduct(e) {
-  const dataId = e.target.dataset.id
-  
-  const inputContainer = document.querySelector(`[data-js="input-container"][data-id="${dataId}"]`)
+  const dataId = e.target.dataset.id;
 
-  inputContainer.remove()
+  const inputContainer = document.querySelector(
+    `[data-js="input-container"][data-id="${dataId}"]`
+  );
 
-  updateTotalPrice()
+  inputContainer.remove();
+
+  updateTotalPrice();
+}
+
+function normalizeFormData () {
+  const inputContainer = document.querySelectorAll('[data-js="input-container"]');
+
+  const arrayElements = [...inputContainer];
+
+  const formData = arrayElements.map((div) => {
+    const name = div.querySelector('[data-js="input-product"]').value;
+    const price = cleanValue(
+      div.querySelector('[data-js="input-price"]').value
+    );
+
+    return {
+      name,
+      price,
+    };
+  });
+
+  return formData
+}
+
+function createOrderObject (formData) {
+  return {
+    products: formData,
+    total: cleanValue(total.innerText),
+  };
+}
+
+function validateFormFields (order) {
+  return order.products.every((product) => product.name && product.price > 0)
+}
+
+async function sendOrder (order) {
+  try {
+    const response = await fetch(url_to_fetch, {
+      method: 'POST',
+      headers: { "Content-Type": "application/json"},
+      body: JSON.stringify(order),
+    })
+    
+    if (!response.ok) {
+      throw new Error
+    }
+
+    redirect()
+  } catch {
+    alert('🥲 Erro ao enviar pedido, tente novamente')
+  }
 }
 
 async function submit(e) {
   e.preventDefault();
-  
-  const inputContainer = document.querySelectorAll('[data-js="input-container"]')
 
-  const arrayElements = [...inputContainer]
+  const formData = normalizeFormData()
 
-  const formData = arrayElements.map((div) => {
-    const name = div.querySelector('[data-js="input-product"]').value
-    const price = cleanValue(div.querySelector('[data-js="input-price"]').value)
+  const order = createOrderObject(formData)
 
-    return { 
-      name,
-      price,
-    }
-  })
-  
-  const order = {
-    products: formData,
-    total: cleanValue(total.innerText)
-  }
-    
-  if (order.products[0].name && order.products[0].price !== 0) {
-    await fetch(url_to_fetch, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(order)
-    }).then(response => response.json())
-      .then(() => redirect())
-      .catch(() => {
-        alert('❌ Todos os campos devem ser preenchidos!')
-      })
-  } else {
-    throw Error()
+  if (!validateFormFields(order)) {
+    alert("❌ Todos os campos devem ser preenchidos!")
+    return;
   }
 
-  return order
+ await sendOrder(order);
 }
 
 function redirect() {
-  location.href = '../home/index.html'
+  location.href = "../home/index.html";
 }
 
 function main() {
-  cloneElement({ id: 0 })
+  cloneElement({ id: 0 });
 }
 
-main()
+main();
+
+///// todo: criar função que -> normaliza os dados
+///// todo: criar função que -> criar obj de pedido
+///// todo: criar função que -> validação dos dados -> every
+///// todo: criar função que -> fetch - save
+
+///// todo: mostrar saldo do cashback
+
+// todo: setup em react
+// todo: migrar projeto para react
--- pages/home/home.js
+++ pages/home/home.js
@@ -3,43 +3,38 @@ const total = document.querySelector('[data-js="total"]')
 
 const url_to_fetch = 'https://606467ecf0919700177859ac.mockapi.io/api/orders'
 
-async function listOrders() {
-  await fetch(url_to_fetch)
+function listOrders() {
+ fetch(url_to_fetch)
     .then(response => response.json())
     .then(orders => {
-
-      getTotalValue(orders)
-
       orders.forEach((order) => {
         order.products.forEach((product) => {
           const tableRow = document.createElement('tr')
           const tableNumber = document.createElement('td')
           const tableProduct = document.createElement('td')
           const tablePrice = document.createElement('td')
+          const tableCashback = document.createElement('td')
 
           tableNumber.innerText = order.id
           tableProduct.innerText = product.name
           tablePrice.innerText = formatCurrency(product.price)
+          tableCashback.innerText = formatCurrency(getCashbackByOrder(order.total))
 
           tableRow.appendChild(tableNumber)
           tableRow.appendChild(tableProduct)
           tableRow.appendChild(tablePrice)
+          tableRow.appendChild(tableCashback)
 
           tableBody.appendChild(tableRow)
         })
+
+        getCashbackTotal(orders.map(order => order.total))
       })
+      
     })
     .catch(error => {
       console.log(error)
     })
-}
-
-function getTotalValue(orders) {
- const totalValue = orders.reduce((acc, order) => {
-    return acc + order.total
-  }, 0)
-
-  total.innerText = formatCurrency(totalValue)
 }
 
 function formatCurrency(value) {
@@ -51,3 +46,51 @@ function formatCurrency(value) {
 
 
 listOrders()
+
+/*
+
+* CASHBACK RULES by ORDER
+
+R$0,00   -> R$50,00   = NÃO TEM CASHBACK
+
+R$50,01  -> R$100,00  = 5% DE CASHBACK
+
+R$100,01 -> R$500,00  = 10% DE CASHBACK
+
+R$500,01 -> INFINITO  = 15% DE CASHBACK
+
+*/
+
+function getCashbackByOrder (total) {
+  const cashback = [];
+
+  if (total < 50) {
+    cashback.push(0);
+  }
+
+  else if (total > 50.01 && total <= 100) {
+    cashback.push((total * 5) / 100);
+  }
+
+  else if (total > 100.01 && total <= 500) {
+    cashback.push((total * 10) / 100);
+  }
+
+  else if (total > 500.01) {
+    cashback.push((total * 15) / 100);
+  }
+
+  return cashback;
+}
+
+function getCashbackTotal(ordersTotals) {
+  const cashbackArray = []
+
+  ordersTotals.forEach(value => cashbackArray.push(getCashbackByOrder(value)))
+
+  const totalCashback = cashbackArray.flat().reduce((acc, total) => {
+    return acc + total
+  }, 0)
+
+  return total.innerText = formatCurrency(totalCashback)
+}
